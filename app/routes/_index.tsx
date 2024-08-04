7cb41294720a8c6d2a06db62242e6065249386df
Riku Aiba <nisaka.rk@gmail.com>
2024-08-05 03:34:40 +0900

    up

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,22 @@
 import type { MetaFunction } from "@remix-run/node";
+import { fetchData } from "@remix-run/react/dist/data";
+import { filter } from "compression";
+import { useState } from "react";
+
+const fetchHistoryData = async (filters: Record<string, any>) => {
+  const queryParams = new URLSearchParams(
+    Object.entries(filters)
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .filter(([key, value]) => value !== "")
+      .map(([key, value]) => [key, value])
+  ).toString();
+  const res = await fetch(`localhost:3000/api/v1/history?${queryParams}`);
+  if (!res.ok) {
+    throw new Error("Faild to fetch data");
+  }
+  return res.json();
+};
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,41 +26,30 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+  const [currentTable, setCurrentTable] = useState<"inventory" | "history">(
+    "inventory"
   );
+  const [historyData, setHistoryData] = useState<any[]>([]);
+  const [inventoryData, setInventoryData] = useState<any[]>([]);
+  const [filters, setFilters] = useState<Record<string, any>>({
+    history_id: "",
+    product_id: "",
+    product_name: "",
+    manufacturer_name: "",
+    supplier_name: "",
+    transaction_date: "",
+    quantity: "",
+    description: "",
+  });
+
+  const updateData = async () => {
+    try {
+      const [history, inventory] = await Promise.all([
+        fetchHistoryData(filters),
+        
+      ])
+    }
+  }
+
+  return <div></div>;
 }
---
+++ app/routes/inventory.tsx
@@ -0,0 +1,16 @@
+import { Grid } from "gridjs-react";
+import "gridjs/dist/theme/mermaid.css";
+
+interface InventoryTableProps {
+    data: any[];
+}
+
+const HisotryTable = () => {
+    const data = 
+}
+
+return (
+    <Grid
+    data
+
+)
